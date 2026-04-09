ef5e5366aa00b0015f125fc167fe6f86774487c3
wilcompute <wiljd4@gmail.com>
2026-04-09 00:03:48 +0000

    α⁻¹ FULLY DECOMPOSED: tree = Gaussian norm, loop = χ of chain complex
    α⁻¹ = (k-1)²+μ² + vq²χ/[χ(Φ₄⁴-1)+q³]
    
    TREE LEVEL: |(k-1)+μi|² = |11+4i|² = 137
      Gaussian integer norm from the SRG eigenvalues
    
    ONE-LOOP: vq²χ/[χ(Φ₄⁴-1)+q³] = 880/24445
      χ = 22 = Euler characteristic of Points→Pairs→Spreads
      Φ₄⁴ = 10000 → (Φ₄⁴-1)/q² = 1111 (repunit in base Φ₄!)
      q³ = 27 = number of spreads = E₆ fundamental dim
      176 = μ²(k-1) = numerator in reduced form
    
    RESULT: 137 + 880/24445 = 669969/4889 = 137.035999182
      CODATA 2022: 137.035999177 ± 21 → 0.2σ
      4889 is prime
    
    ANALYTIC TORSION:
      T² = gf/(v-k+1) = 360/29 (regularized determinant)
      T = √(360/29) = 3.523

---
+++ exploration/w33_alpha_from_chain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+α⁻¹ = (k-1)²+μ² + vq²χ/[χ(Φ₄⁴-1)+q³] = 137 + 880/24445 = 137.035999182
+
+Tree: |(k-1)+μi|² = |11+4i|² = 137 (Gaussian norm)
+Loop: vq²χ/[χ(Φ₄⁴-1)+q³] where χ=22 (Euler char of chain complex)
+1111 = (Φ₄⁴-1)/q² = repunit in base 10
+M_eff = 1111 + 3/22 = (Φ₄⁴-1)/q² + q/χ = 24445/22
+α⁻¹ = 137 + 176/4889 = 669969/4889 (4889 is prime)
+176 = μ²(k-1) = s²(k-1)
+
+Analytic torsion: T = √(gf/(v-k+1)) = √(360/29)
+"""
