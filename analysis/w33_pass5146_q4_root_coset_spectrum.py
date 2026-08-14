c7c037e18c68857160da8101f3b330371a6531d6
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-08-14 13:27:10 -0400

    Pass5146 wrapper: q4 derivative spectrum

---
+++ analysis/w33_pass5146_q4_root_coset_spectrum.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from analysis.w33_collision_clean_runner import run
+if __name__=='__main__':run('q4_root_coset_spectrum.py','PART_W33_PASS5146_Q4_ROOT_COSET_SPECTRUM.json',5146)
