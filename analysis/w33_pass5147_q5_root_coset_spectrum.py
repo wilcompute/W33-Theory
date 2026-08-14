7120169298a655a58dd6907070906974e38a542e
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-08-14 13:27:28 -0400

    Pass5147 wrapper: q5 derivative spectrum

---
+++ analysis/w33_pass5147_q5_root_coset_spectrum.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from analysis.w33_collision_clean_runner import run
+if __name__=='__main__':run('q5_root_coset_spectrum.py','PART_W33_PASS5147_Q5_ROOT_COSET_SPECTRUM.json',5147)
