263dab4af6a38aef2ee84249906d6b524ddef4d3
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-08-14 13:28:04 -0400

    Pass5149 wrapper: q3 cubic theta moment

---
+++ analysis/w33_pass5149_q3_cubic_theta_moment.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from analysis.w33_collision_clean_runner import run
+if __name__=='__main__':run('q3_cubic_theta_moment.py','PART_W33_PASS5149_Q3_CUBIC_THETA_MOMENT.json',5149,'PART_W33_PASS5148_Q3_THETA_TRIANGLE_CURVATURE.json')
