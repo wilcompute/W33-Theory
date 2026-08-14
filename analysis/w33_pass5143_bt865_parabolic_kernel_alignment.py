998b27d17419b8227113ed67e623e155b3c5f5a5
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-08-14 13:26:14 -0400

    Pass5143 wrapper: BT865 parabolic alignment

---
+++ analysis/w33_pass5143_bt865_parabolic_kernel_alignment.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from analysis.w33_collision_clean_runner import run
+if __name__=='__main__':run('bt865_parabolic_kernel_alignment.py','PART_W33_PASS5143_BT865_PARABOLIC_ALIGNMENT.json',5143)
