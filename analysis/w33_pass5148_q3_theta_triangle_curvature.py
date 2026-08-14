5f019f9671c601940003c5323eb02eee8cb139fd
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-08-14 13:27:46 -0400

    Pass5148 wrapper: q3 theta triangle curvature

---
+++ analysis/w33_pass5148_q3_theta_triangle_curvature.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from analysis.w33_collision_clean_runner import run
+if __name__=='__main__':run('q3_theta_triangle_curvature.py','PART_W33_PASS5148_Q3_THETA_TRIANGLE_CURVATURE.json',5148)
