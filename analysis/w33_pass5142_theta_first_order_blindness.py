8f0a416986371785b17b1bfbf7a2241a389d1990
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-08-14 13:25:58 -0400

    Pass5142 wrapper: theta spectral blindness

---
+++ analysis/w33_pass5142_theta_first_order_blindness.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from analysis.w33_collision_clean_runner import run
+if __name__=='__main__':run('theta_first_order_blindness.py','PART_W33_PASS5142_THETA_FIRST_ORDER_BLINDNESS.json',5142)
