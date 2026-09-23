bad0119c085cad26a89a847190236c8333db57ec
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-09-23 02:51:19 -0400

    Correct hybrid frontier expected TeX line

--- tests/test_w33_shared_frontier_tail_integrity.py
+++ tests/test_w33_shared_frontier_tail_integrity.py
@@ -49,7 +49,7 @@ def test_shared_frontier_tail_is_linewise_valid_and_unique():
         r"\input{analysis/PASS20260923_execute_all5_hesse_toe_compiler_frontier_insert}%",
         r"\input{analysis/PASS20260923_dark_strange_hybrid81_insert}%",
         r"\input{analysis/PASS20260923_execute_all5_plus3_physics_insert}%",
-        r"\\input{analysis/PASS20260923_hybrid_lie_golay_calibration_insert}%",
+        r"\input{analysis/PASS20260923_hybrid_lie_golay_calibration_insert}%",
     ]
     for item in expected:
         assert item in lines
