3e130947bea85c4c286bbc9d9408130c9a2851e7
wilcompute <67532012+wilcompute@users.noreply.github.com>
2026-09-23 03:58:44 -0400

    Guard canonical-to-current H27 gauge bridge import

--- tests/test_w33_shared_frontier_tail_integrity.py
+++ tests/test_w33_shared_frontier_tail_integrity.py
@@ -53,6 +53,7 @@ def test_shared_frontier_tail_is_linewise_valid_and_unique():
         r"\input{analysis/PASS20260923_cubic_jacobian_rank_stratification_insert}%",
         r"\input{analysis/PASS20260923_cubic_fourier54_alignment_insert}%",
         r"\input{analysis/PASS20260923_diagonal_phase_weld_insert}%",
+        r"\input{analysis/PASS20260923_e6id_current_h27_gauge_bridge_insert}%",
     ]
     for item in expected:
         assert item in lines
